7e5c5a020ef68bfb2607dbf847a961bdae1e9ed7
Valentin Richet <valentin.richet@gmail.com>
2020-01-06 19:47:41 +0100

    Added Winston logger and a little error handler (need work)

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,22 @@
 import bodyParser from 'body-parser';
 import express, { Express, NextFunction, Request, Response } from "express";
+import * as winston from 'winston';
 import connectToDb from './lib/database';
 import Environment from "./lib/environment";
 import { UserRoute } from "./routes/users";
+import ErrorHandler from "./utils/error";
+import expressWinston = require('express-winston');
 
 const app: Express = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(expressWinston.logger({
+    transports: [
+        new winston.transports.Console({})
+    ]
+}));
+app.use(ErrorHandler);
 
 app.use("/users", UserRoute);
 
